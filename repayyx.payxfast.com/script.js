af04fbaadf518da456f8e4cab9dc1c7b0cda99cd
FAST PAY <mstaysab96@gmail.com>
2024-10-16 05:53:23 +0600

    Update script.js

--- repayyx.payxfast.com/script.js
+++ repayyx.payxfast.com/script.js
@@ -38,7 +38,9 @@ document.getElementById('confirmButton').onclick = function() {
         alert('Please enter a UTR number before submitting.');
     }
 };
-
+function radio() {
+  var x = document.getElementById("rdaacls").value;
+}
 // Close modal when clicking outside
 window.onclick = function(event) {
     if (event.target == document.getElementById('copyModal')) {
